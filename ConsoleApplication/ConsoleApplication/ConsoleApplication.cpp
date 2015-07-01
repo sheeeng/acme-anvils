88f7fb2fcb016d13d58ca3b93fc4ce8288a4afef
Leonard Lee <sheeeng@gmail.com>
2015-07-01 10:55:25 +0200

    Add output lines.

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -2,10 +2,13 @@
 //
 
 #include "stdafx.h"
-
+#include <conio.h>  // _getch()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+    _tprintf(_T("Output from Acme Anvils project.\n"));
+    _tprintf(_T("Press any key to continue.\n"));
+    _getch();
+    return 0;
 }
 
